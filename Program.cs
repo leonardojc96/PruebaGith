42da64c5a8782886d7c9c7fb83fafa611522fa68
Leonardo Cardozo <leonardojc85@hotmail.com>
2020-02-06 04:44:48 -0300

    Agregado un writeline

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace PruebaGith
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("hola");
         }
     }
 }
